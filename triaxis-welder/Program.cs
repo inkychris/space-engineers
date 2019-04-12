dd0476053beffaeade2da7eec1bf6301009cb998
inkychris <chris@inkyspider.co.uk>
2019-04-12 18:58:16 +0100

    Completed initial triaxis welder program

--- triaxis-welder/Program.cs
+++ triaxis-welder/Program.cs
@@ -20,24 +20,73 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
+        private IMyProgrammableBlock self;
         private TriaxisPistonGroup pistons;
         private IMyShipWelder welder;
+        private ArgParser parser;
+        private string[] commands;
+        private int commandIndex;
 
         public Program()
         {
+            self = GetBlock<IMyProgrammableBlock>("Triaxis Welder Controller");
             pistons = LargeTriaxisPistonGroup("X Axis", "Y Axis", "Z Axis");
             welder = GetBlock<IMyShipWelder>("Welder");
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            var parser = new ArgParser(argument);
-            if (parser.Contains("home")) {
+            if (argument != "")
+            {
+                commands = new string[] { argument };
+                commandIndex = 0;
+                parser = new ArgParser(commands[commandIndex]);
+            }
+
+            if (parser.Contains("home"))
+            {
                 pistons.GoTo(new TriaxisVector(), parser.Float("-v|--velocity", 0.5f));
                 return;
             }
+
+            if (parser.Contains("custom"))
+            {
+                commands = self.CustomData.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                commandIndex = 0;
+                parser = new ArgParser(commands[commandIndex]);
+            }
+
+            if (commandIndex < commands.Length)
+            {
+                Echo("Running command" + commandIndex);
+                Runtime.UpdateFrequency = UpdateFrequency.Update10;
+                if (GoTo())
+                {
+                    commandIndex++;
+                    if (commandIndex == commands.Length)
+                        return;
+                    parser = new ArgParser(commands[commandIndex]);
+                    Echo("Incrementing cmd index");
+                    return;
+                }
+            }
+            else
+            {
+                Runtime.UpdateFrequency = UpdateFrequency.None;
+                Echo("Done!");
+            }
+        }
+
+        public bool Home()
+        {
+            return pistons.Stopped();
+        }
+
+        public bool GoTo()
+        {
             var target = new TriaxisVector(parser.Float("x"), parser.Float("y"), parser.Float("z"));
-            pistons.GoTo(target, parser.Float("v", 0.5f));
+            pistons.GoTo(target, parser.Float("-v|--velocity", 0.5f));
+            return pistons.CurrentPosition().Equals(target);
         }
     }
 }
