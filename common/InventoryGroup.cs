e527eab608e3d1d0103de8c02aa99ba1e814807b
inkychris <chris@inkyspider.co.uk>
2019-04-13 16:10:09 +0100

    Added Balance method to Inventory Group to spread item distribution across inventories

--- common/InventoryGroup.cs
+++ common/InventoryGroup.cs
@@ -23,13 +23,21 @@ namespace IngameScript
 
         public class InventoryGroup
         {
+            private MyGridProgram program;
             private List<IMyInventory> inventories;
 
-            public InventoryGroup(List<IMyEntity> entitylist)
+            public InventoryGroup(MyGridProgram program, List<IMyEntity> entitylist)
             {
+                this.program = program;
                 inventories = new List<IMyInventory>();
                 foreach (IMyEntity entity in entitylist)
                     inventories.Add(entity.GetInventory());
+            }
+
+            private InventoryGroup(MyGridProgram program, List<IMyInventory> inventorylist)
+            {
+                this.program = program;
+                inventories = inventorylist;
             }
 
             public VRage.MyFixedPoint ItemAmount(MyItemType item)
@@ -49,25 +57,37 @@ namespace IngameScript
                 return count;
             }
 
+            public static VRage.MyFixedPoint TransferBetween(IMyInventory source, IMyInventory target, MyItemType itemType, VRage.MyFixedPoint? amount = null)
+            {
+                if (source == target)
+                    return 0;
+                List<MyInventoryItem> items = new List<MyInventoryItem>();
+                source.GetItems(items, sourceItem => sourceItem.Type == itemType);
+                if (items.Count == 0)
+                    return 0;
+                VRage.MyFixedPoint totalTransferred = 0;
+                foreach (MyInventoryItem item in items)
+                {
+                    VRage.MyFixedPoint amountBefore = target.GetItemAmount(itemType);
+                    source.TransferItemTo(target, item, amount - totalTransferred);
+                    totalTransferred += target.GetItemAmount(itemType) - amountBefore;
+                }
+                return totalTransferred;
+            }
+
             public VRage.MyFixedPoint TransferTo(IMyInventory target, MyItemType itemType, MyFixedPoint? amount = null)
             {
                 VRage.MyFixedPoint amountToTransfer;
                 if (amount == null)
                     amountToTransfer = ItemAmount(itemType);
                 else
-                    amountToTransfer = (VRage.MyFixedPoint)amount;
+                    amountToTransfer = ((VRage.MyFixedPoint)amount).ToIntSafe();
                 VRage.MyFixedPoint amountTransferred = 0;
                 foreach (IMyInventory inventory in inventories)
                 {
                     if (amountTransferred >= amountToTransfer)
                         return amountTransferred;
-                    var maybeItem = inventory.FindItem(itemType);
-                    if (maybeItem == null)
-                        continue;
-                    var item = (MyInventoryItem)maybeItem;
-                    VRage.MyFixedPoint amountBefore = target.GetItemAmount(itemType);
-                    inventory.TransferItemTo(target, item, amountToTransfer - amountTransferred);
-                    amountTransferred += target.GetItemAmount(itemType) - amountBefore;
+                    amountTransferred += TransferBetween(inventory, target, itemType, amountToTransfer - amountTransferred);
                 }
                 return amountTransferred;
             }
@@ -87,6 +107,34 @@ namespace IngameScript
                     amountTransferred += TransferTo(target, itemType, amountToTransfer - amountTransferred);
                 }
                 return amountTransferred;
+            }
+
+            private InventoryGroup Excluding(IMyInventory inventory)
+            {
+                List<IMyInventory> newList = new List<IMyInventory>();
+                foreach (IMyInventory existing in inventories)
+                    if (existing != inventory)
+                        newList.Add(existing);
+                return new InventoryGroup(program, newList);
+            }
+
+            public void Balance(MyItemType item)
+            {
+                var balancedAmount = (VRage.MyFixedPoint)((double)ItemAmount(item) / inventories.Count);
+                InventoryGroup invGroup = this;
+                foreach (IMyInventory inventory in inventories)
+                {
+                    var currentAmount = inventory.GetItemAmount(item);
+                    invGroup = invGroup.Excluding(inventory);
+                    if (currentAmount < balancedAmount)
+                    {
+                        invGroup.TransferTo(inventory, item, balancedAmount - currentAmount);
+                        continue;
+                    }
+                    var extraAmount = currentAmount - balancedAmount;
+                    foreach (IMyInventory otherInventory in invGroup.inventories)
+                        extraAmount -= TransferBetween(inventory, otherInventory, item, extraAmount);
+                }
             }
         }
     }
